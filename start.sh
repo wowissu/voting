33a9ebc73602776a72726f44b77cbd644480aa13
wowissu <bbgbbg0123@gmail.com>
2023-08-18 01:39:54 +0800

    feat: download should put outside

---
+++ start.sh
@@ -0,0 +1,3 @@
+lsof -i TCP:3333 | awk 'NR!=1 {print $2}' | xargs kill;
+kill -9 $(ps aux | grep '\snext-router-worker' | awk '{print $2}')
+nohup next start --port 3333 &> /home/opt/nohup.txt &
--- update.sh
+++
@@ -1,9 +0,0 @@
-cd /home/opt/voting;
-
-git pull;
-npm install;
-npm run build;
-
-lsof -i TCP:3333 | awk 'NR!=1 {print $2}' | xargs kill;
-# kill -9 $(ps aux | grep '\snext-router-worker' | awk '{print $2}')
-nohup next start --port 3333 &> /home/opt/nohup.txt &
